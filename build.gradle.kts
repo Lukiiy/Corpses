dd77bd224cb5e188e12e15302f6437e45e4abc54
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-04-15 19:06:03 -0300

    Shadowjar + tasks
    * Added Gradle tasks to process paper-plugin.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,38 @@
 plugins {
     java
+    id("com.gradleup.shadow") version "9.4.1"
 }
 
 group = "me.lukiiy"
 version = "1.0-SNAPSHOT"
+description = "Displays corpses of those who perished."
 
 repositories {
+    mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.9-R0.1-SNAPSHOT")
     implementation(files("lib/ManneInventory-1.0-SNAPSHOT.jar"))
+}
+
+tasks {
+    shadowJar {
+        mergeServiceFiles()
+        archiveClassifier.set("")
+        minimize()
+    }
+
+    build { dependsOn(shadowJar) }
+
+    processResources {
+        val props = mapOf("version" to version, "desc" to rootProject.description)
+
+        inputs.properties(props)
+        filteringCharset = "UTF-8"
+        filesMatching("paper-plugin.yml") { expand(props) }
+    }
 }
 
 java {
